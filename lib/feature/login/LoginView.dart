de6d7daea498ff78ea84fe686946edd45ee581ef
Thiago Augusto Da Costa Silva <thyago_casablancas@hotmail.com>
2022-03-07 13:30:44 -0300

    Updates

--- lib/feature/login/LoginView.dart
+++ lib/feature/login/LoginView.dart
@@ -1,4 +1,7 @@
+import 'package:design_system_poc/design_system_poc.dart';
 import 'package:flutter/material.dart';
+import 'package:design_system_poc/src/widgets/main_button.dart';
+import 'package:design_system_poc/src/widgets/main_textInput.dart';
 
 class LoginView extends StatefulWidget {
   @override
@@ -7,7 +10,39 @@ class LoginView extends StatefulWidget {
 
 class LoginViewState extends State<LoginView> {
   @override
-  Widget build(BuildContext context) => Container(
-        child: Center(child: Text("LOREM IPSUM")),
+  void initState() {
+    super.initState();
+  }
+
+  void _testClick() {
+    print("BOTÃO FOI CLICADO!");
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Center(
+                child: Column(
+              children: [
+                Text("LOREM IPSUM DOLOR SIT AMET"),
+                MainTextInput(
+                    controller: TextEditingController(), labelText: "LOGIN"),
+                MainTextInput(
+                    controller: TextEditingController(), labelText: "SENHA")
+              ],
+            )),
+            MainButton(
+                height: 50,
+                width: MediaQuery.of(context).size.width - 32,
+                labelText: "ENTRAR",
+                backgroundColor: Colors.blueAccent,
+                borderColor: secondaryDarkColor,
+                action: () => _testClick()),
+          ],
+        )),
       );
 }
